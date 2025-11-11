c9a8aad300560c5b62315f04286fa7c296b10f3d
Eclissi-di-Lune <eclissidilune@proton,me>
2025-11-11 01:27:18 +0100

    Test 6

--- netlify/functions/glitch-effect.js
+++ netlify/functions/glitch-effect.js
@@ -131,7 +131,7 @@ exports.handler = async (event) => {
                                                                   ::.                                                           
                                                                   ::                                                            
                                                                   :.                                                            
-                                                                  .                                                                                                                                                                                                      
+                                                                   :                                                                                                                                                                                                     
         `;
     
         const o_to_second_bug = `
